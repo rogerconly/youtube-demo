85181fdc8f6f56675d2025c158d6196cb824a6cb
Roger <459074126@qq.com>
2020-12-03 20:54:29 +0800

    modified new templatefile

---
+++ youtube-demo/ConstantsTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-demo
+//
+//  Created by Roger Yuen on 2020/12/3.
+//
+
+import Foundation
+
+//struct Constansts {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "  https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constansts.PLAYLIST_ID)&key=\(Constansts.API_KEY)"
+//}
